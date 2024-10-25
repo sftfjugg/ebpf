82ae091c8da224276dc99036cc127eed98324998
Lorenz Bauer <lmb@isovalent.com>
2024-10-25 08:02:43 -0700

    sys: add windows support
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

---
+++ internal/sys/strings_other.go
@@ -0,0 +1,13 @@
+//go:build !windows
+
+package sys
+
+import "golang.org/x/sys/unix"
+
+func ByteSliceToString(s []byte) string {
+	return unix.ByteSliceToString(s)
+}
+
+func ByteSliceFromString(s string) ([]byte, error) {
+	return unix.ByteSliceFromString(s)
+}
---
+++ internal/sys/syscall_other.go
@@ -0,0 +1,36 @@
+//go:build !windows
+
+package sys
+
+import (
+	"runtime"
+	"unsafe"
+
+	"github.com/cilium/ebpf/internal/errno"
+	"github.com/cilium/ebpf/internal/unix"
+)
+
+// BPF wraps SYS_BPF.
+//
+// Any pointers contained in attr must use the Pointer type from this package.
+func BPF(cmd Cmd, attr unsafe.Pointer, size uintptr) (uintptr, error) {
+	// Prevent the Go profiler from repeatedly interrupting the verifier,
+	// which could otherwise lead to a livelock due to receiving EAGAIN.
+	if cmd == BPF_PROG_LOAD || cmd == BPF_PROG_RUN {
+		maskProfilerSignal()
+		defer unmaskProfilerSignal()
+	}
+
+	for {
+		r1, _, errNo := unix.Syscall(unix.SYS_BPF, uintptr(cmd), uintptr(attr), size)
+		runtime.KeepAlive(attr)
+
+		// As of ~4.20 the verifier can be interrupted by a signal,
+		// and returns EAGAIN in that case.
+		if errNo == errno.EAGAIN && cmd == BPF_PROG_LOAD {
+			continue
+		}
+
+		return r1, errno.Error(errNo)
+	}
+}
